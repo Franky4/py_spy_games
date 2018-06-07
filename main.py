fe3ddbbb16afc725b2adf4cd699f94ea1c3ce8b1
Aleksey Kozlov <mail@reg65.com>
2018-06-08 07:21:51 +1100

    PEP8

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from Data.config import TOKEN
 from time import sleep
 
@@ -27,24 +28,47 @@ class MyVkClient:
         else:
             return response.json()['response']['items']
 
+    def get_group_info_by_id(self, group_id=None):
+        if group_id is not None:
+            self.params['group_id'] = group_id
+        self.params['fields'] = 'name,members_count'
+        response = requests.get('https://api.vk.com/method/groups.getById', self.params)
+        return response.json()['response']
+
+    def save_to_json(self, data_to_save):
+        with open('data.json', 'w') as f:
+            json.dump(data_to_save, f, ensure_ascii=False, indent=2)
+
 
 def main():
+    vk_id = '5030613'
     vk_data = MyVkClient(TOKEN)
-    my_friends = vk_data.get_friends_by_id('5030613')
-    a = vk_data.get_groups_by_id('5030613')
+    my_friends = vk_data.get_friends_by_id(vk_id)
+    #  user_vk_groups = vk_data.get_groups_by_id(vk_id)
     count = len(my_friends)
-
-    a = set(a)
-    print(a)
+    unique_vk_groups = set(vk_data.get_groups_by_id(vk_id))
     for friend in my_friends:
         print(count)
-        b = vk_data.get_groups_by_id(friend)
-        if b is not None:
-            b = set(b)
-            a -= b
+        friend_vk_groups = vk_data.get_groups_by_id(friend)
+        if friend_vk_groups is not None:
+            friend_vk_groups = set(friend_vk_groups)
+            unique_vk_groups -= friend_vk_groups
             sleep(0.35)
         count = count - 1
-    print(a)
+
+    unique_vk_groups = list(unique_vk_groups)
+    print(unique_vk_groups)
+
+    new_list = []
+    for ug in unique_vk_groups:
+        new_dict = {}
+        temp_dict = vk_data.get_group_info_by_id(ug)[0]
+        new_dict['id'] = temp_dict['id']
+        new_dict['name'] = temp_dict['name']
+        new_dict['members_count'] = temp_dict['members_count']
+        new_list.append(new_dict)
+
+    vk_data.save_to_json(new_list)
 
 
 if __name__ == '__main__':
